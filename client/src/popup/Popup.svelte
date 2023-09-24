4088ea231398aad69c03b322947d45e5cf2b7a35
Olaseni Bello <Olasenibello38@gmail.com>
2023-09-24 11:56:12 -0400

    something

--- client/src/popup/Popup.svelte
+++ client/src/popup/Popup.svelte
@@ -27,28 +27,73 @@
 </script>
 
 <main>
-    <div>
+    <div class="popup-container">
         {#if isBad == true}
-            <p id='bad-text'>SUSPECIOUS CONTENT DETECTED!</p>
+            <p class="header"> Monas</p>
+            <p id='bad-text'>SUSPICIOUS CONTENT DETECTED!</p>
         {:else}
+            <p class="header"> Monas</p>
             <p id='good-text'>Nothing Wrong</p>
         {/if}
     </div>
 </main>
 
 <style>
-    *{
-        padding: 0px;
-        margin: 0px;
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
     }
-    #bad-text{
-        font-size: 1rem;
-        color: white;
-        background-color: red;
+  
+    body {
+      font-family: 'Arial', sans-serif;
+      background-color: #f0f0f0;
     }
-    #good-text{
-        font-size: 1rem;
-        color: white;
-        background-color: green;
+  
+    main {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 60vh;
     }
-</style>
+  
+    .popup-container {
+      text-align: center;
+      padding: 20px;
+      border-radius: 8px;
+      background-color: #fff;
+      border-radius: 20px;
+      transition: transform 0.3s ease-in-out;
+    }
+  
+    #bad-text {
+      font-size: 1.5rem;
+      color: #ff5252;
+      background-color: #fff; 
+      padding: 15px 30px;
+      border-radius: 4px;
+      margin-bottom: 20px;
+    }
+  
+    #good-text {
+      font-size: 1.5rem;
+      color: #4caf50;;
+      background-color: white;
+      padding: 15px 30px;
+      border-radius: 4px;
+    }
+  
+    .popup-container:hover {
+      transform: scale(1.05);
+    }
+
+    .header{
+        padding-top: 10px;
+        font-weight : bold ;
+        font-family: 'JetBrains Mono', monospace;
+        font-size: 2rem;
+        color: blue;
+    }
+  </style>
+  
+  
