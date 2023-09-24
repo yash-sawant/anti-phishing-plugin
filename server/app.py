c13b0b6ad58c9adb17d4f7f6e05a969f03946202
yash-sawant <yash.sawant96@gmail.com>
2023-09-23 22:11:32 -0400

    flask app creted, hugging face api connected

--- server/app.py
+++ server/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, jsonify
+from inference import query
 
 app = Flask(__name__)
 
-@app.route('/query', methods=['GET'])
-def query():
-    query_param = request.args.get('q', '')
+
+@app.route('/classify_email', methods=['POST'])
+def classify_email():
+    query_param = request.args.get('text')
     response = f"Received query: {query_param}"
+    if query_param:
+        result = query(query_param)
+    else:
+        result = "No text received"
+
     return jsonify({'message': response})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
--- server/inference.py
+++ server/inference.py
@@ -5,10 +5,16 @@ from secrets import API_TOKEN
 API_URL = "https://api-inference.huggingface.co/models/foghlaimeoir/phishing-DistilBERT"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-def query(payload):
+def query(text):
+    payload = {
+        "inputs": text,
+    }
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-output = query({
-    "inputs": "I like you. I love you",
-})
+
+if __name__ == "__main__":
+
+    output = query('Are you interested in joining a pyramid scheme?')
+
+    print(output)
